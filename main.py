137a150985cb8e5fad400957d1af801d5c29c427
luxkatana <taseen.bibi@gmail.com>
2023-10-07 15:44:53 +0200

    different sentences

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 from flask import Flask, render_template
-import subprocess, re
+import subprocess
+import re
+import os
+def is_raspberry() -> bool:
+    return True if os.name == 'posix'\
+        and 'Raspberry' in execute_command("cat /proc/cpuinfo") else\
+            False
+def execute_command(cmd: str) -> str:
+    if os.name == 'nt':
+        return subprocess.run(['C:\Windows\System32\WindowsPowerShell\\v1.0\powershell.exe', '-Command', cmd],
+                              capture_output=True)\
+                                  .stdout\
+                                      .decode()\
+                                          .strip()
+    return subprocess.run(cmd, shell=True, capture_output=True).stdout.decode().strip()
 
 app = Flask(__name__)
-execute_and_return = lambda cmd: subprocess.run(cmd, shell=True, capture_output=True).stdout.decode()
-def get_ip():
-    p = subprocess.run(["ip", "addr", "show"], capture_output=True)\
-    .stdout\
-    .decode()
-
+def get_ip() -> tuple[str]:
+    if os.name == 'nt':
+        return (execute_command("(Get-NetIPAddress -AddressFamily IPv4 -InterfaceAlias (Get-NetAdapter | Where-Object {$_.Status -eq 'Up'} | Select-Object -First 1).Name).IPAddress"),)
+        
+    p = execute_command("ip addr show")
     def verify_ip(ipaddr: str):
         exclusion = frozenset(['127.0.0.1'])
-        result = subprocess.run(f'ping -c 1 -W 2 {ipaddr}'
-                                .split(), capture_output=True)
-        if ipaddr.strip() in exclusion: return False
-        if result is not None and re.findall(r"1 received", result.stdout.decode()):
+        result = execute_command(f'ping -c 1 -W 2 {ipaddr}')
+        if ipaddr in exclusion:
+            return False
+        if result is not None and re.findall(r"1 received", result):
             #TODO: Change received = 1 to 1 received
             return True
         return False
@@ -31,8 +44,19 @@ def get_ip():
 def hoi():
     ips = get_ip()
     if len(ips) > 0:
-        return render_template('index.html', status='connected', ip=ips[0],
-                               ssid=execute_and_return('iwgetid -r'))
+        if os.name != 'nt':
+            return render_template('index.html', status='connected', ip=ips[0],
+                                ssid=execute_command('iwgetid -r'),
+                                raspberry=is_raspberry(),
+                                os_=os.name)
+        else:
+            SSID = execute_command('(get-netconnectionProfile).Name')\
+                .split('\n')
+            return render_template('index.html', status='connected',
+                                   ip=ips[0],
+                                   ssid=SSID[0],
+                                   raspberry=False,
+                                   os_=os.name)
     else:
         return render_template('index.html', status='disconnected')
 
