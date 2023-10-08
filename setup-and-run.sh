de2e82a5cccde4432cbd645122b3fde3913d8130
luxkatana <57036931+luxkatana@users.noreply.github.com>
2023-10-08 10:56:21 +0200

    Update setup-and-run.sh

--- setup-and-run.sh
+++ setup-and-run.sh
@@ -3,7 +3,7 @@ if command -v python3 &>/dev/null; then
     $python3_path -m pip install -r requirements.txt
 
     echo "Benodigheden voor server klaar"
-    gunicorn --bind 0.0.0.0:9090 main:app
+    waitress-serve --listen=0.0.0.0:9090 main:app
     # $python3_path $(echo "$(pwd)/main.py")
 else
     echo "Python 3 is not niet geinstalleerd."
