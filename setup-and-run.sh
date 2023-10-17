2a72d482206edcd44f8de75c0859c31b0755a4f9
luxkatana <taseen@raspberrypi>
2023-10-17 11:39:29 +0100

    $1 = host to ip

--- setup-and-run.sh
+++ setup-and-run.sh
@@ -4,7 +4,7 @@ if command -v python3 &>/dev/null; then
     $python3_path -m pip install -r requirements.txt
 
     echo "Benodigheden voor server klaar"
-    waitress-serve --listen=0.0.0.0:9090 main:app
+    waitress-serve --listen=$1:9090 main:app
     # $python3_path $(echo "$(pwd)/main.py")
 else
     echo "Python 3 is nog niet geinstalleerd."
